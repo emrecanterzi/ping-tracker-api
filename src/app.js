727dde47ebb02e81c14c1507eaaf2e5bee2c3c20
emrecanterzi <terzi.6112@gmail.com>
2023-08-03 16:15:33 +0300

    #11 cors policy updated

--- src/app.js
+++ src/app.js
@@ -10,7 +10,12 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
 
 app.use(indexRouter);
 
